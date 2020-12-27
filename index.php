86cf1719aff84b523229f4dab9c7320f7e6a65e0
Prosanta <prosantapaulbsmrstu@gmail.com>
2020-12-27 11:45:20 +0600

    user_create

--- index.php
+++ index.php
@@ -16,6 +16,25 @@
 <!-- users table section End -->
 <!-- create user section start -->
 <section class="create-user">
+  <form action="$_SERVER['PHP_SELF']" method="post">
+          <div class="input-group">
+              <label for="name">Name:</label>
+              <input type="text" name="name" id="name">
+          </div>
+          <div class="input-group">
+              <label for="email">Email:</label>
+              <input type="email" name="email" id="email">
+          </div>
+          <div class="input-group">
+              <label for="phone">Phone No.:</label>
+              <input type="text" name="phone" id="phone">
+          </div>
+          <div class="input-group">
+              <label for="image">Image:</label>
+              <input type="file" name="image" id="image">
+          </div>
+          <input type="submit" name="createUser" value="Create">
+      </form>
 </section>
 <!-- create user section end -->
 <!-- edit user section start -->
