8baa792e8c7a75ace995a6921a10602963d6faec
Prosanta <prosantapaulbsmrstu@gmail.com>
2020-12-27 11:22:28 +0600

    index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- style -->
+    <link rel="stylesheet" href="style.css">
+    <title>PHP CRUD</title>
+</head>
+<body>
+<!-- users table section start -->
+<section class="user-table">
+    <table border="1">
+    </table>
+</section>
+<!-- users table section End -->
+<!-- create user section start -->
+<section class="create-user">
+</section>
+<!-- create user section end -->
+<!-- edit user section start -->
+<section class="create-user">
+</section>
+<!-- edit user section end -->
+
+</body>
+</html>
